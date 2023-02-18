e0b3335c076ccd1e7373ce3456eba30756990582
Trent Ferguson <trent.r.ferguson@gmail.com>
2023-02-18 14:07:57 -0500

    Added initial model classes

---
+++ PrsBackEnd/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace PrsBackEnd.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+
+        public bool IsReviewer { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+    }
+}
--- PrsBackEnd/Program.cs
+++ PrsBackEnd/Program.cs
@@ -18,6 +18,7 @@ if (app.Environment.IsDevelopment())
 
 // app.UseHttpsRedirection();
 
+
 app.UseAuthorization();
 
 app.MapControllers();
